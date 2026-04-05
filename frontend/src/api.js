f51e9173e42853fde1d03814fb05cf68d1593884
Sameer-11-16 <sameeransari00000@gmail.com>
2026-04-06 03:23:05 +0530

    update api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://leave-management-8xqz.onrender.com/' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
